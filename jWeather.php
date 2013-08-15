a4155e534af6f4f8dd22bb7bcde52b56b8c90072
jtiq <jtiq@ya.ru>
2013-08-15 00:08:06 -0700

    Create jWeather.php

---
+++ jWeather.php
@@ -0,0 +1,56 @@
+<?php
+
+Class jWeather {
+  private $cache = 7200;
+
+  public function load($code) {
+    $data = @file_get_contents("http://export.yandex.ru/weather-ng/forecasts/$code.xml");
+
+    if (!is_null($data) && strlen($data)) {
+      $xml = @simplexml_load_string($data);
+      $i = 'image-v3';
+
+      return array(
+        'temp' => $xml->fact->temperature,
+        'image' => "http://yandex.st/weather/1.1.76.1/i/icons/30x30/".$xml->fact->$i.".png",
+        'time' => (time() + mt_rand(-30, 30))
+      );
+    }
+
+    return null;
+  }
+
+  public function get($codes) {
+    $return = array();
+
+    if (is_array($codes)) {
+      foreach ($codes as $code) {
+        $jwdata = get_vars('jweather_'.$code);
+        $return[$code] = $jwdata;
+
+        if (($jwdata['time'] + $this->cache) < time()) {
+          $jwdata = $this->load($code);
+
+          if (is_array($jwdata)) {
+            $return[$code] = $jwdata;
+          }
+        }
+      }
+    } else {
+      $jwdata = get_vars('jweather_'.$code);
+      $return[$code] = $jwdata;
+
+      if (($jwdata['time'] + $this->cache) < time()) {
+        $jwdata = $this->load($code);
+
+        if (is_array($jwdata)) {
+          $return[$code] = $jwdata;
+        }
+      }
+    }
+
+    return $return;
+  }
+}
+
+?>
